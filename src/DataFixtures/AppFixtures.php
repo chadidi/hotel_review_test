f6242b26f2d88299c8358153067437afac3e693d
Abdellah Chadidi <chadidi97@gmail.com>
2019-12-11 21:21:11 +0100

    Seed 5 hotels with each having 7 reviews

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,9 @@
 
 namespace App\DataFixtures;
 
+use Faker\Factory;
+use App\Entity\Hotel;
+use App\Entity\Review;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -9,8 +12,24 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create();
+
+        for ($i = 0; $i < 5; $i++) {
+            $hotel = new Hotel();
+            $hotel->setName($faker->company);
+            $hotel->setCreatedAt($created_at = $faker->dateTime('-1 month'));
+            $manager->persist($hotel);
+            for ($i = 0; $i < 7; $i++) {
+                $review = new Review();
+                // $review->setHotelId($hotel->getId());
+                $review->setTitle($faker->sentence);
+                $review->setBody($faker->text(500));
+                $review->setRating($faker->randomFloat(2, 0, 5));
+                $review->setCreatedAt($faker->dateTimeBetween($created_at, 'now'));
+                $hotel->addReview($review);
+                $manager->persist($review);
+            }
+        }
 
         $manager->flush();
     }
