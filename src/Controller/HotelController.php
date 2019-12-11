d37837f06c61d4e80593640376c674a755238244
Abdellah Chadidi <chadidi97@gmail.com>
2019-12-11 21:47:08 +0100

    Create today review endpoint

--- src/Controller/HotelController.php
+++ src/Controller/HotelController.php
@@ -2,8 +2,45 @@
 
 namespace App\Controller;
 
+use App\Repository\HotelRepository;
+use App\Repository\ReviewRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HotelController extends AbstractController
 {
+    private $hotelRepository;
+    private $reviewRepository;
+
+    public function __construct(HotelRepository $hotelRepository, ReviewRepository $reviewRepository)
+    {
+        $this->hotelRepository = $hotelRepository;
+        $this->reviewRepository = $reviewRepository;
+    }
+
+    /**
+     * @Route("/{hotelId}/today/review", name="hotel_today_review", methods={"GET"})
+     */
+    public function todayReview($hotelId): JsonResponse
+    {
+        $hotel = $this->hotelRepository->findOneBy(['id' => $hotelId]);
+
+        if (!$hotel) {
+            return new JsonResponse([
+                'error' => [
+                    'message' => 'The hotel id was not found.',
+                ]
+            ], Response::HTTP_NOT_FOUND);
+        }
+
+        $count = $hotel->getReviews()->count();
+        // dd($count);
+        $today_review = $this->reviewRepository->getRandomEntity($hotelId)->toArray();
+
+        return new JsonResponse([
+            'today_review' => $today_review,
+        ], Response::HTTP_OK);
+    }
 }
